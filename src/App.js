fccdbd1b4b08d2ad25bdd27156f761e4b02f510d
ARUN PRASAD <arun.dot007@gmail.com>
2022-12-21 17:40:58 +0530

    netlify commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <main className="w-full h-full ">
+      {/* {github homepage for package json} */}
+      {/* "homepage": "https://anbesivamayam.github.io/PanDictionary", */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
